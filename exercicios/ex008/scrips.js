5dbf485bc76501b0604490a80af91574950a0c49
thiago1818 <thiago1818@gmail.com>
2022-05-31 07:11:11 -0300

    Estruturas Condicionais

---
+++ exercicios/ex008/scrips.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+console.log(`A velocidade é ${vel}`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. 'MULTADO'`)
+} else {
+    console.log(`Dirija com cuidado!`)
+}
